7d6fe8dcd20578452c43b9f0eb007cae520ca71a
Uislan Cruz <uislansk8@hotmail.com>
2022-04-22 18:56:24 -0300

    Update Campo.java

--- src/br/com/cod3r/com/modelo/Campo.java
+++ src/br/com/cod3r/com/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.cod3r.com.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.com.excecao.ExplosaoException;
+
 public class Campo {
 	
 	private final int linha;
@@ -41,5 +43,40 @@ public class Campo {
 		return false;
 		}
 	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if (minado) {
+				throw new ExplosaoException();
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+				
+			}
+			
+			return true;
+			
+		}else {
+			
+			return false;
+		}
+		
+		
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+
 
 }
+
