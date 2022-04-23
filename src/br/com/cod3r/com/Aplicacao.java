bee0e25d4e1174be883fffb1bebbf32396f23952
Uislan Cruz <uislansk8@hotmail.com>
2022-04-23 18:48:01 -0300

    Create Aplicacao.java

---
+++ src/br/com/cod3r/com/Aplicacao.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.com;
+
+import br.com.cod3r.com.modelo.Tabuleiro;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		
+		System.out.println(tabuleiro);
+	}
+
+}
