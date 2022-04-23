d5ba4550518984955eb377ea47359e553409b4df
Uislan Cruz <uislansk8@hotmail.com>
2022-04-23 19:41:11 -0300

    Visao do Tabuleiro

--- src/br/com/cod3r/com/Aplicacao.java
+++ src/br/com/cod3r/com/Aplicacao.java
@@ -1,18 +1,15 @@
 package br.com.cod3r.com;
 
 import br.com.cod3r.com.modelo.Tabuleiro;
+import br.com.cod3r.com.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		Tabuleiro tabuleiro =new Tabuleiro(6,6,6);
 		
+		new TabuleiroConsole(tabuleiro);
 		
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		tabuleiro.abrir(3, 3);
-		
-		System.out.println(tabuleiro);
 	}
 
 }
