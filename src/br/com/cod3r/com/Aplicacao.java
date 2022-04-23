ed3fe23e36f666e88920834e3a89b791c29917cb
Uislan Cruz <uislansk8@hotmail.com>
2022-04-23 19:29:11 -0300

    Update Aplicacao.java

--- src/br/com/cod3r/com/Aplicacao.java
+++ src/br/com/cod3r/com/Aplicacao.java
@@ -6,9 +6,11 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
-		tabuleiro.abrir(3, 3);
+		
+		
 		tabuleiro.alternarMarcacao(4, 4);
 		tabuleiro.alternarMarcacao(4, 5);
+		tabuleiro.abrir(3, 3);
 		
 		System.out.println(tabuleiro);
 	}
