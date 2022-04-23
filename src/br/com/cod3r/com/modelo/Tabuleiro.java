3c1c3c72469111c1cda780a95ec3d294a75a8253
Uislan Cruz <uislansk8@hotmail.com>
2022-04-23 18:42:37 -0300

    Update Tabuleiro.java

--- src/br/com/cod3r/com/modelo/Tabuleiro.java
+++ src/br/com/cod3r/com/modelo/Tabuleiro.java
@@ -1,7 +1,9 @@
 package br.com.cod3r.com.modelo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 	
@@ -9,7 +11,62 @@ public class Tabuleiro {
 	private int colunas;
 	private int minas;
 	
-	private final List<Campo> campo = new ArrayList<Campo>();
+	private final List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
 	
+	private void gerarCampos() {
+		for (int linhaFor = 0; linhaFor < linhas; linhaFor++) {
+			for (int colunaFor = 0; colunaFor < colunas; colunaFor++) {
+				campos.add(new Campo(linhaFor,colunaFor));
+			}
+			
+		}
+	}
+	
+	private void associarVizinhos() {
+		for(Campo c1:campos) {
+			for(Campo c2 : campos) {
+			c1.adicionarVizinho(c2);
+			}
+		}
+	
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random()* campos.size());
+			campos.get(aleatorio).minar();
+		}while(minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado()); 
+		
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		
+	}
+	
+	public String toString() {
+		return "";
+	
+	}
 	 
 }
