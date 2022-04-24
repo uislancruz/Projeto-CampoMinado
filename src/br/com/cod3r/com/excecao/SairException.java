5f373a6da8cfea0f505253b89d7fed4814a00e22
Uislan Cruz <uislansk8@hotmail.com>
2022-04-24 13:21:30 -0300

    Criando o ciclo do jogo

---
+++ src/br/com/cod3r/com/excecao/SairException.java
@@ -0,0 +1,9 @@
+package br.com.cod3r.com.excecao;
+
+public class SairException extends RuntimeException{
+
+	private static final long serialVersionUID = 2L;
+	
+	
+
+}
--- src/br/com/cod3r/com/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/com/visao/TabuleiroConsole.java
@@ -1,13 +1,51 @@
 package br.com.cod3r.com.visao;
 
+import java.util.Scanner;
+
+import br.com.cod3r.com.excecao.ExplosaoException;
+import br.com.cod3r.com.excecao.SairException;
 import br.com.cod3r.com.modelo.Tabuleiro;
 
 public class TabuleiroConsole {
 	
 	private Tabuleiro tabuleiro;
+	private Scanner entrada = new Scanner(System.in);
 	
 	public TabuleiroConsole(Tabuleiro tabuleiro) {
 		this.tabuleiro = tabuleiro;
+		
+		executarJogo();
+	}
+
+	private void executarJogo() {
+		try {
+			boolean continuar = true;
+			cicloDoJogo();			
+			
+			while(continuar) {
+				System.out.println("Outra partida? (S/n)");
+				String resposta = entrada.nextLine();
+				
+				if("n".equalsIgnoreCase(resposta)){
+					continuar = false;
+				}else {
+					tabuleiro.reiniciar();
+				}
+			}
+		} catch (SairException e) {
+			System.out.println("Tchau!!!");
+		}finally {
+			entrada.close();
+		}
+	}
+
+	private void cicloDoJogo() {
+		try {
+			System.out.println("Voce ganhou!!!!");
+		} catch (ExplosaoException e) {
+			System.out.println("Voce perdeu!");
+		}
+		
 	}
 
 }
