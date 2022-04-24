de9e66aa6c48b1aabfe847fae0cb8923e7a796b0
Uislan Cruz <uislansk8@hotmail.com>
2022-04-24 12:50:27 -0300

    Update TabuleiroConsole.java

--- src/br/com/cod3r/com/visao/TabuleiroConsole.java
+++ src/br/com/cod3r/com/visao/TabuleiroConsole.java
@@ -1,5 +1,13 @@
 package br.com.cod3r.com.visao;
 
+import br.com.cod3r.com.modelo.Tabuleiro;
+
 public class TabuleiroConsole {
+	
+	private Tabuleiro tabuleiro;
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
 
 }
